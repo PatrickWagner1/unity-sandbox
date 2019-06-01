bff4946c29922c68762559fe2d9154d0e660bd51
Patrick <patrickwagner1@bitbucket.org>
2019-06-01 20:00:04 +0200

    removed submesh method and clean up code
    Removed the submesh method to simplify the creation of the only mesh.

--- Assets/Scripts/DiamondSquareGenerator.cs
+++ Assets/Scripts/DiamondSquareGenerator.cs
@@ -1,19 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Generates a mesh and random terrain using the Diamond Square algorithm
 /// </summary>
-public class DiamondSquareGenerator : MonoBehaviour
-{
-
-	private const int MaxMeshSize = 254;
-
-    /// <summary>
-    /// Material object of the mesh
-    /// /// </summary>
-    private Material material;
+public class DiamondSquareGenerator : MonoBehaviour {
 
     /// <summary>
     /// The total size of the mesh
@@ -21,112 +14,68 @@ public class DiamondSquareGenerator : MonoBehaviour
     private int size = 10;
 
     /// <summary>
-    /// Size of the sub meshes
-    /// </summary>
-    private int subMeshSize = 100;
-
-    /// <summary>
     /// Initial method
     /// /// </summary>
-    void Awake()
-    {
-        this.material = GetComponent<MeshRenderer>().material;
+    void Awake () {
 
-        this.createMesh(this.size);
+        this.createMesh (this.size, 1.0f, 15);
 
-
-        //gameObject.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
-        gameObject.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);        
-        gameObject.transform.Rotate(Vector3.right, 90);
+        gameObject.transform.localScale = new Vector3 (0.05f, 0.05f, 0.05f);
     }
 
     /// <summary>
     /// Update method that gets called once per frame
     /// </summary>
-    void Update()
-    {
+    void Update () {
 
     }
 
     /// <summary>
-    /// Creates a mesh of sub meshes with the given width and height
+    /// Creates a mesh with the given size. The heights will be calculated with the diamond square algorithm with the given rough and seed.
     /// </summary>
-    /// <param name="width">Mesh width</param>
-    /// <param name="height">Mesh height</param>
-    void createMesh(int size)
-    {
-		float[,] heights = this.diamondSquare(size, 1.0f, 20);
+    /// <param name="size"></param>
+    /// <param name="rough"></param>
+    /// <param name="seed"></param>
+    void createMesh (int size, float rough, int seed) {
+        int totalSize = (int) Mathf.Pow (2, size) + 1;
+        float[, ] heights = this.diamondSquare (size, rough, seed);
+        this.eliminateNegativeValues (heights);
 
-		int totalSize = (int)Mathf.Pow(2, size) + 1;
-		//int subSize = (totalSize / MaxMeshSize) + 1;
-        //int totalSubMesh = totalSize / subSize;
-        int subSize = totalSize;
-        int totalSubMesh = 1;
-
-        for (int xSub = 0; xSub < totalSubMesh; xSub++)
-        {
-            for (int ySub = 0; ySub < totalSubMesh; ySub++)
-            {
-                this.createSubMesh(xSub, ySub, subSize, totalSize, heights);
-            }
-        }
-    }
-
-    /// <summary>
-    /// Creates a sub mesh 
-    /// </summary>
-    /// <param name="xSubMeshCount"></param>
-    /// <param name="ySubMeshCount"></param>
-    void createSubMesh(int xSubMeshCount, int ySubMeshCount, int subSize, int totalSize, float[,] heights)
-    {
-        GameObject meshGameObject = new GameObject();
-        meshGameObject.transform.SetParent(gameObject.transform);
-        meshGameObject.AddComponent<MeshRenderer>().material = this.material;
-        meshGameObject.name = "SubMesh(" + xSubMeshCount + "," + ySubMeshCount + ")";
-        MeshFilter meshFilter = meshGameObject.AddComponent<MeshFilter>();
-        Mesh mesh = new Mesh();
+        GameObject meshGameObject = new GameObject ();
+        meshGameObject.transform.SetParent (gameObject.transform);
+        meshGameObject.AddComponent<MeshRenderer> ().material = GetComponent<MeshRenderer>().material;
+        meshGameObject.name = "SubMesh(" + totalSize + "x" + totalSize + ")";
+        MeshFilter meshFilter = meshGameObject.AddComponent<MeshFilter> ();
+        Mesh mesh = new Mesh ();
         mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
         meshFilter.mesh = mesh;
 
-        Vector3[] vertices = new Vector3[subSize * subSize];
-        Vector2[] uvs = new Vector2[subSize * subSize];
-        int[] triangles = new int[6 * ((subSize - 1) * (subSize - 1))];
-
-        int xOffset = xSubMeshCount;
-        int yOffset = ySubMeshCount;
+        Vector3[] vertices = new Vector3[totalSize * totalSize];
+        Vector2[] uvs = new Vector2[totalSize * totalSize];
+        int[] triangles = new int[6 * ((totalSize - 1) * (totalSize - 1))];
 
         int triangleIndex = 0;
 
-		int testX;
-		int testY;
-		// Fill vertices and uvs
-		for (int y = 0; y < subSize; y++)
-        {
-            for (int x = 0; x < subSize; x++)
-            {
-                int index = (y * subSize) + x;
+        // Fill vertices and uvs
+        for (int z = 0; z < totalSize; z++) {
+            for (int x = 0; x < totalSize; x++) {
+                int index = (z * totalSize) + x;
 
-				int globalX = x + (xSubMeshCount * subSize) - xOffset;
-				int globalY = - y - (ySubMeshCount * subSize) + yOffset;
-				testX = globalX;
-				testY = globalY;
-
-                vertices[index] = new Vector3(globalX, globalY, heights[globalX, -globalY]);
-                uvs[index] = new Vector2(((float)(xSubMeshCount * subSize + x) / (float)totalSize),
-                                         ((float)(ySubMeshCount * subSize + y) / (float)totalSize));
+                float y = heights[x, z];
+                vertices[index] = new Vector3 (x, y, z);
+                uvs[index] = new Vector2 (((float) x),
+                    ((float) z));
             }
         }
 
         // Fill triangles
-		for (int y = 0; y < subSize - 1; y++)
-        {
-            for (int x = 0; x < subSize - 1; x++)
-            {
-                int index = (y * subSize) + x;
+        for (int z = 0; z < totalSize - 1; z++) {
+            for (int x = 0; x < totalSize - 1; x++) {
+                int index = (z * totalSize) + x;
 
                 int topLeft = index;
                 int topRight = topLeft + 1;
-                int bottomLeft = topLeft + subSize;
+                int bottomLeft = topLeft + totalSize;
                 int bottomRight = bottomLeft + 1;
 
                 triangles[triangleIndex++] = topLeft;
@@ -141,68 +90,77 @@ public class DiamondSquareGenerator : MonoBehaviour
         mesh.vertices = vertices;
         mesh.uv = uvs;
         mesh.triangles = triangles;
-        mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
+        mesh.RecalculateNormals ();
+        mesh.RecalculateBounds ();
     }
 
-	private float[,] diamondSquare(int size, float rough, int seed)
-	{
-		Random.InitState((int)(Random.value*250.0f));
-		int depth = (int) Mathf.Pow(2, size);
-		int totalSize = depth + 1;
-		float[,] map = new float[totalSize, totalSize];
-		map[0, 0] = Random.value;
-		map[0, depth] = Random.value;
-		map[depth, 0] = Random.value;
-		map[depth, depth] = Random.value;
+    private float[, ] diamondSquare (int size, float rough, int seed) {
+        Random.InitState ((int) (Random.value * 250.0f));
+        int depth = (int) Mathf.Pow (2, size);
+        int totalSize = depth + 1;
+        float[, ] map = new float[totalSize, totalSize];
+        map[0, 0] = Random.value;
+        map[0, depth] = Random.value;
+        map[depth, 0] = Random.value;
+        map[depth, depth] = Random.value;
 
-		float average;
-		float range = 500.0f;
-		int halfSide;
-		
-		for (int sideLength = totalSize - 1; sideLength > 1; sideLength /= 2)
-		{
-			halfSide = sideLength / 2;
+        float average;
+        float range = 500.0f;
+        int halfSide;
 
-			// Diamond step
-			for (int x = 0; x < depth; x += sideLength)
-			{
-				for (int z = 0; z < depth; z += sideLength)
-				{
-					average = (map[x, z] + map[x + sideLength, z]
-					+ map[x, z + sideLength] + map[x + sideLength, z + sideLength]) / 4.0f
-					+ (Random.value * (range * 2.0f)) - range;
-					map[x + halfSide, z + halfSide] = average;
-				}
-			}
+        for (int sideLength = totalSize - 1; sideLength > 1; sideLength /= 2) {
+            halfSide = sideLength / 2;
 
-			// Square step
-			for (int x = 0; x < depth; x += halfSide)
-			{
-				for (int z = (x + halfSide) % sideLength; z < depth; z += sideLength)
-				{
-					average = (map[(x - halfSide + depth) % depth, z]
-					+ map[(x + halfSide) % depth, z]
-					+ map[x, (z + halfSide) % depth]
-					+ map[x, (z - halfSide + depth) % depth]) / 4.0f
-					+ (Random.value * (range * 2.0f)) - range;
+            // Diamond step
+            for (int x = 0; x < depth; x += sideLength) {
+                for (int z = 0; z < depth; z += sideLength) {
+                    average = (map[x, z] + map[x + sideLength, z] +
+                            map[x, z + sideLength] + map[x + sideLength, z + sideLength]) / 4.0f +
+                        (Random.value * (range * 2.0f)) - range;
+                    map[x + halfSide, z + halfSide] = average;
+                }
+            }
 
-					map[x, z] = average;
+            // Square step
+            for (int x = 0; x < depth; x += halfSide) {
+                for (int z = (x + halfSide) % sideLength; z < depth; z += sideLength) {
+                    average = (map[(x - halfSide + depth) % depth, z] +
+                            map[(x + halfSide) % depth, z] +
+                            map[x, (z + halfSide) % depth] +
+                            map[x, (z - halfSide + depth) % depth]) / 4.0f +
+                        (Random.value * (range * 2.0f)) - range;
 
-					if (x == 0)
-					{
-						map[depth, z] = average;
-					}
+                    map[x, z] = average;
 
-					if (z == 0) {
-						map[x, depth] = average;
-					}
-				}
-			}
-			
-			range -= range * 0.5f * rough;
-		}
+                    if (x == 0) {
+                        map[depth, z] = average;
+                    }
 
-		return map;
-	}
+                    if (z == 0) {
+                        map[x, depth] = average;
+                    }
+                }
+            }
+
+            range -= range * 0.5f * rough;
+        }
+
+        return map;
+    }
+
+    private void eliminateNegativeValues (float[, ] map) {
+        int xSize = map.GetLength (0);
+        int zSize = map.GetLength (1);
+        for (int x = 0; x < xSize; x++) {
+            for (int z = 0; z < zSize; z++) {
+                if (map[x, z] < 0) {
+                    map[x, z] = 0;
+                }
+            }
+        }
+    }
+
+    public void OnValueChanged (float value) {
+        this.createMesh (this.size, 1.0f, (int) value);
+    }
 }
---
+++ Assets/Scripts/MouseScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript1 : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    private void mouseEvent () {
+        if (Input.GetMouseButtonDown (0)) {
+            Debug.Log ("Linke Maustaste gedrückt!");
+            Vector3 currentPos = Input.mousePosition;
+            double currentXPos = Input.mousePosition.x;
+            double currentYPos = Input.mousePosition.y;
+            double currentZPos = Input.mousePosition.z;
+            // can be - and + depending on movement up or down
+            double movement = Input.GetAxis ("Mouse ScrollWheel");
+            if (movement != 0) {
+
+                // check if current pos - movement <= 0
+                if (true) {
+                    // water
+
+                }
+
+            }
+        }
+    }
+}
